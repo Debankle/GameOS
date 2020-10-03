77951fd273fed5f73c998f9dd3554055da8563e5
Debankle <oliver.debankle@gmail.com>
2020-10-03 17:06:49 +1000

    defines unsigned types

---
+++ libc/stdint.h
@@ -0,0 +1,9 @@
+#ifndef GAMEOS_STDINT_H
+#define GAMEOS_STDINT_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+#endif // GAMEOS_STDINT_H
