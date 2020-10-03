2c201d93e6e5faa3e361b3b23476e287bbd8746f
Debankle <oliver.debankle@gmail.com>
2020-10-03 17:07:12 +1000

    Organised more

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,6 +1,14 @@
 #include "../drivers/display.h"
+#include "../drivers/serial.h"
+
+void kinit() {
+    display_init();
+    serial_init();
+}
 
 void kmain() {
-    display_init();
+    kinit();
+
     kprint("Hello World!\n");
+    write_serial(SERIAL_COM1_BASE, 'p');
 }
