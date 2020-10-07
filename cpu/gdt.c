b54e34f5f5b28cc1594f263a46cea7128e451a01
Debankle <oliver.debankle@gmail.com>
2020-10-08 01:20:28 +1000

    It finally works dynamically i want to end life its been 7 hours god im fucking dumb

--- cpu/gdt.c
+++ cpu/gdt.c
@@ -1,14 +1,28 @@
 #include "gdt.h"
 #include "../libc/stdint.h"
 
-// TODO: update gdt entry creation for more control
+#define SEG_BASE 0
+#define SEG_LIMIT 0x000FFFFF
 
-#define SEG_BASE 0x0000
-#define SEG_LIMIT 0xFFFF
-#define PL0 0x0
-#define PL3 0x3
-#define CODE 0xA
-#define DATA 0x2
+// Yes I know these can be replaced with static hex numbers but this 
+// way it is obvious what bits are what because this GDT has been 
+// giving me massive headaches so i made it as dynamic as possible
+#define GRAN(x) ((x) << 0x3)
+#define SIZE(x) ((x) << 0x2)
+#define SYS_AVAILABLE(x) ((x) << 0x1)
+#define PRESENT(x) ((x) << 0x7)
+#define PRIVILAGE(x) ((x & 0x03) << 0x5)
+#define DESCRIPTOR(x) ((x) << 0x4)
+#define EXEC(x) ((x) << 0x3)
+#define DIRECTION(x) ((x) << 0x2)
+#define READWRITE(x) ((x) << 0x1)
+#define ACCESSED(X) ((X) << 0x0)
+
+#define FLAG GRAN(1) | SIZE(1) | (0 << 0x1) | (0 << 0x0)
+#define CODE_0 PRESENT(1) | PRIVILAGE(0) | DESCRIPTOR(1) | EXEC(1) | DIRECTION(0) | READWRITE(1) | ACCESSED(0)
+#define DATA_0 PRESENT(1) | PRIVILAGE(0) | DESCRIPTOR(1) | EXEC(0) | DIRECTION(0) | READWRITE(1) | ACCESSED(0)
+#define CODE_3 PRESENT(1) | PRIVILAGE(3) | DESCRIPTOR(1) | EXEC(1) | DIRECTION(0) | READWRITE(1) | ACCESSED(0)
+#define DATA_3 PRESENT(1) | PRIVILAGE(3) | DESCRIPTOR(1) | EXEC(0) | DIRECTION(0) | READWRITE(1) | ACCESSED(0)
 
 struct gdt_ptr {
     uint16_t limit;
@@ -16,63 +30,46 @@ struct gdt_ptr {
 } __attribute__((packed));
 typedef struct gdt_ptr gdt_ptr_t;
 
-struct gdt_entry {
+struct gdt_descriptor {
     uint16_t limit;
     uint16_t base_low;
     uint8_t base_mid;
     uint8_t access;
-    uint8_t granularity;
+    uint8_t flags;
     uint8_t base_high;
 } __attribute__((packed));
-typedef struct gdt_entry gdt_entry_t;
+typedef struct gdt_descriptor gdt_descriptor_t;
 
-gdt_entry_t gdt_entries[5];
-
-// void gdt_create_entry(uint32_t n, uint16_t base, uint16_t limit, uint8_t pl, uint8_t type) {
-//     gdt_entry_t entry;
-//     entry.limit = (SEG_LIMIT & 0xFFFF);
-//     entry.base_low = (SEG_BASE & 0xFFFF);
-//     entry.base_mid = (SEG_BASE & 0xFF);
-//     entry.access = (0x01 << 7) | ((pl & 0x03) << 5) | (0x01 << 4) | (type & 0x0F);
-//     entry.granularity |= (0x01 << 7) | (0x01 << 6) | 0x0F;
-//     entry.base_high = (SEG_BASE & 0xFF);
-//     gdt_entries[n] = entry;
-// }
-
-void gdt_create_entry(uint32_t n, uint8_t access, uint8_t granularity) {
-    gdt_entries[n].limit = (SEG_LIMIT & 0xFFFF);
-    gdt_entries[n].base_low = (SEG_BASE & 0xFFFF);
-    gdt_entries[n].base_mid = ((SEG_BASE >> 16) & 0xFF);
-    gdt_entries[n].base_high = ((SEG_BASE >> 24) & 0xFF);
-    gdt_entries[n].access = access;
-    gdt_entries[n].granularity = granularity;
-}
+gdt_descriptor_t gdt[5];
 
 void load_gdt(uint32_t);
+
+void create_descriptor(uint32_t n, uint32_t base, uint32_t limit, uint16_t access, uint8_t flag)
+{
+    gdt_descriptor_t descriptor;
+
+    descriptor.limit = limit;
+    descriptor.base_low = base;
+    descriptor.base_mid = base & 0x000FF;
+    descriptor.access = access;
+    descriptor.flags = ((flag << 4) & 0xF0) | (limit & 0xF);
+    descriptor.base_high = base & 0xFF;
+
+    gdt[n] = descriptor;
+}
+
 
 void gdt_init()
 {
     gdt_ptr_t gdt_base;
     gdt_base.limit = sizeof(uint64_t) * 5;
-    gdt_base.base = (uint32_t)&gdt_entries;
+    gdt_base.base = (uint32_t)&gdt;
 
-    gdt_create_entry(0, 0, 0);
-    gdt_create_entry(1, 0x9A, 0xCF);
-    gdt_create_entry(2, 0x92, 0xCF);
-    gdt_create_entry(3, 0xFA, 0xCF);
-    gdt_create_entry(4, 0xF2, 0xCF);
-
-    // gdt_create_entry(0, 0, 0, 0, 0);
-    // gdt_create_entry(1, SEG_BASE, SEG_LIMIT, PL0, CODE);
-    // gdt_create_entry(2, SEG_BASE, SEG_LIMIT, PL0, DATA);
-    // gdt_create_entry(3, SEG_BASE, SEG_LIMIT, PL3, CODE);
-    // gdt_create_entry(4, SEG_BASE, SEG_LIMIT, PL3, DATA);
-
-    // gdt_entries[0] = 0x0000000000000000;
-    // gdt_entries[1] = 0x00CF9A000000FFFF;
-    // gdt_entries[2] = 0x00CF92000000FFFF;
-    // gdt_entries[3] = 0x00CFFA000000FFFF;
-    // gdt_entries[4] = 0x00CFF2000000FFFF;
+    create_descriptor(0, 0, 0, 0, 0);
+    create_descriptor(1, SEG_BASE, SEG_LIMIT, CODE_0, FLAG);
+    create_descriptor(2, SEG_BASE, SEG_LIMIT, DATA_0, FLAG);
+    create_descriptor(3, SEG_BASE, SEG_LIMIT, CODE_3, FLAG);
+    create_descriptor(4, SEG_BASE, SEG_LIMIT, DATA_3, FLAG);
 
     load_gdt((uint32_t)&gdt_base);
 }
