9ffdb19bb92c44bdb234f632a2f7be763be510c0
Debankle <oliver.debankle@gmail.com>
2020-10-07 20:01:27 +1000

    Added gdt to init

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,9 +1,11 @@
 #include "../drivers/display.h"
 #include "../drivers/serial.h"
+#include "../cpu/gdt.h"
 
 void kinit() {
     display_init();
     serial_init();
+    gdt_init();
 }
 
 void kmain() {
